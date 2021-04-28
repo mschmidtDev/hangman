6317cb19a18eed8b65d3db36946d90fb253995b4
Smashltpv <m.schmidt@basecom.de>
2021-04-28 17:17:02 +0200

    Changes to main

--- main.c
+++ main.c
@@ -10,9 +10,23 @@
 #include "timer.h"
 #include "game_state.h"
 #include "input.h"
+#include "game_state.h"
 
 int main()
 {
+    struct guess this_guess;
+
+    this_guess = new_game();
+
+    start_timer();
+
+    do {
+        this_guess = input_letter(this_guess);
+
+        print_result(this_guess);
+
+    }while (this_guess.mistakes < 10);
+
 
     return 0;
 }
